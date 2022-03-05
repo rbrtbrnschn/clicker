5e79e1601d3dacc39671352dc7dff495ae1d70e4
Robert Bornschein <robertbornscheinofficial@gmail.com>
2022-03-05 12:51:44 +0100

    feat: +prestige model

---
+++ src/stores/prestige/prestige.store.ts
@@ -0,0 +1,24 @@
+import { action, thunk } from "easy-peasy";
+import { PrestigeModel } from "./prestige.model";
+
+export const initialPrestigeData: PrestigeModel = {
+  prestigeCount: 0,
+  tokenCount: 0,
+  setPrestigeCount: action((state,prestigeCount)=>{
+    state.prestigeCount = prestigeCount;
+  }),
+  setTokenCount: action((state,tokenCount)=>{
+    state.tokenCount= tokenCount;
+  }),
+  disptachPrestige: thunk((actions,_,helpers)=>{
+    const addedTokens = helpers.getStoreState().base.totalClicks / 1000;
+    actions.setPrestigeCount(helpers.getState().prestigeCount + 1);
+    actions.setTokenCount(helpers.getState().tokenCount + addedTokens);
+    helpers.getStoreActions().utils.reset();
+  }),
+
+  reset: thunk((actions,_,helpers)=>{
+    actions.setPrestigeCount(0);
+    actions.setTokenCount(0);
+  })
+}
