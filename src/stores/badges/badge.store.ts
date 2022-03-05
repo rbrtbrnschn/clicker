c6cec09cd7c3a8c9c74882b143bcb1fb4b3866ee
Robert Bornschein <robertbornscheinofficial@gmail.com>
2022-03-05 11:55:01 +0100

    refactor: effectOn -> thunkOn

--- src/stores/badges/badge.store.ts
+++ src/stores/badges/badge.store.ts
@@ -1,25 +1,19 @@
 import { BadgesModel } from './badges.model'
 import { badges } from './badges'
-import { action, unstable_effectOn } from 'easy-peasy'
+import { action, actionOn, thunkOn, unstable_effectOn } from 'easy-peasy'
 import { toast } from 'react-toastify'
 export const initialBadgesData: BadgesModel = {
   badges,
   completeBadge: action((state, label) => {
     const badge = state.badges.find((badge) => badge.label === label)
-    if (!badge) return
+    if (!badge) return console.log('couldnt find badge')
     badge.achieved = true
+    console.log('completed badged')
   }),
-  onAchieved: unstable_effectOn(
-    [(state) => state.badges],
-    (actions, change, helpers) => {
-      const [curr] = change.current
-      const [prev] = change.prev
-      const diff = curr.filter((b) => !prev.includes(b))
-      const achievedBadge = diff[0]
-      if (!achievedBadge) return
-
-      toast.success(`Earned ${achievedBadge.label} badge.`)
-      return undefined
+  onCompleteBadge: thunkOn(
+    (actions) => actions.completeBadge,
+    (state, payload, helpers) => {
+      toast.success(`Earned ${payload.payload} badge.`)
     }
   ),
 }
