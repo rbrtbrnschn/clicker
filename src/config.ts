bd0527d082388fd453253f7d9369b9805867ae24
Robert Bornschein <robertbornscheinofficial@gmail.com>
2022-03-06 08:48:15 +0100

    Feat/config (#11)
    * feat: perks cost increase
    
    * refactor: perks - badges
    
    * fix: perks not resseting on reset
    
    * feat: +config | fix: reset prestige on reset

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+interface IConfig {
+  base: {
+    clickStrengthBase: number;
+    clicks: number;
+  }
+}
+export const config: IConfig = {
+  base: {
+    clickStrengthBase: 0.1,
+    clicks: 0,
+  },
+}
